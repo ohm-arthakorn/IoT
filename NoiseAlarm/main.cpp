a090a4435ac2c863286dd1ec48e258a893252e4d
Arthakorn Na-rard <95751222+ohm-arthakorn@users.noreply.github.com>
2024-10-10 13:16:19 +0700

    Add files via upload

--- NoiseAlarm/main.cpp
+++ NoiseAlarm/main.cpp
@@ -1,23 +1,7 @@
+// หรือว่ากูทำ Decibel Meter ดีวะ ??
 #include <Arduino.h>
 #include <WiFi.h>
 #include <PubSubClient.h>
-
-// ? Variable Definition
-const int sampleWindow = 50;
-int sample;
-const char* ssid = "Arthakorn"; // * WIFI ssid 
-const char* password = "123456789"; // * Password WIFI
-const char* mqtt_server = "broker.hivemq.com";
-const int mqtt_port = 1883;
-const char* mqtt_Client = "Arthakorn";
-const char* mqtt_username = "";
-const char* mqtt_password = "";
-WiFiClient espClient;
-PubSubClient client(espClient);
-long lastMsg = 0;
-int value = 0;
-char msg[100];    
-String DataString;
 
 // ? Pin Definition
 #define SENSOR_PIN A0
@@ -26,39 +10,67 @@ String DataString;
 #define Yellow_LED 7
 #define Red_LED 8
 #define Buzzer 12
+#define Subscribe "SoundSensor/subscribe"
+#define Publish "SoundSensor/publish"
 
-void callback(char* topic, byte* payload, unsigned int length){
+// ? Variable Definition
+const int sampleWindow = 50;
+int sample;
+const char *ssid = "Arthakorn";     // * WIFI ssid
+const char *password = "123456789"; // * Password WIFI
+const char *mqtt_server = "broker.hivemq.com";
+const int mqtt_port = 1883;
+const char *mqtt_Client = "Arthakorn";
+const char *mqtt_username = "";
+const char *mqtt_password = "";
+WiFiClient espClient;
+PubSubClient client(espClient);
+long lastMsg = 0;
+int value = 0;
+char msg[100];
+String DataString;
+
+void callback(char *topic, byte *payload, unsigned int length)
+{
   Serial.print("Message arrived [");
   Serial.print(topic);
   Serial.print("]");
   String message;
-  for(int i = 0; i< length; i++){
+  for (int i = 0; i < length; i++)
+  {
     message = message + char(payload[i]);
   }
   Serial.println(message);
-  if(String(topic) == "sound/value"){
-    if(message == "ON"){
+  if (String(topic) == Publish)
+  {
+    if (message == "ON")
+    {
       Serial.println("LED ON");
     }
-    else if(message == "OFF"){
+    else if (message == "OFF")
+    {
       Serial.println("LED OFF");
     }
   }
 }
 
-// * Function to connect internet with board 
-void reconnect(){
-  while (!client.connected()){
+// * Function to connect internet with board
+void reconnect()
+{
+  while (!client.connected())
+  {
     Serial.print("Attempting MQTT connection...");
-  if(client.connect(mqtt_Client, mqtt_username, mqtt_password)){
-    Serial.println("Connected!");
-    client.subscribe("sound/value");
-  }
-  else{
-    Serial.print("failed, rc=");
-    Serial.print(client.state());
-    Serial.println("Try again in 3 seconds");
-    delay(3000);
+    if (client.connect(mqtt_Client, mqtt_username, mqtt_password))
+    {
+      Serial.println("Connected!");
+      client.subscribe(Publish);
+    }
+    else
+    {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println("Try again in 3 seconds");
+      delay(3000);
     }
   }
 }
@@ -71,12 +83,14 @@ void setup()
   pinMode(Red_LED, OUTPUT);
   pinMode(Blue_LED, OUTPUT);
   pinMode(Buzzer, OUTPUT);
-  Serial.begin(115200);
-  Serial.println();;
+  Serial.begin(9600);
+  Serial.println();
+  ;
   Serial.print("Connecting to ");
   Serial.println(ssid);
   WiFi.begin(ssid, password);
-  while(WiFi.status() != WL_CONNECTED){
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(500);
     Serial.print(".");
   }
@@ -86,13 +100,12 @@ void setup()
   Serial.println(WiFi.localIP());
   client.setServer(mqtt_server, mqtt_port);
   client.setCallback(callback);
-  client.subscribe("sound/value");
+  client.subscribe(Publish);
 }
 
-
-
-// * function สำหรับการแสดงผลไฟ LED Bar 
-void LightBar(byte Blue_INPUT, byte Green_INPUT, byte Yellow_INPUT, byte Red_INPUT){
+// * function สำหรับการแสดงผลไฟ LED Bar
+void LightBar(byte Blue_INPUT, byte Green_INPUT, byte Yellow_INPUT, byte Red_INPUT)
+{
   digitalWrite(Blue_LED, Blue_INPUT);
   delay(10);
   digitalWrite(Green_LED, Green_INPUT);
@@ -102,7 +115,7 @@ void LightBar(byte Blue_INPUT, byte Green_INPUT, byte Yellow_INPUT, byte Red_INP
   digitalWrite(Red_LED, Red_INPUT);
 }
 
-// * VOID LOOP RIGHT HERE 
+// * VOID LOOP RIGHT HERE
 void loop()
 {
   unsigned long startMillis = millis(); // Start of sample window
@@ -110,6 +123,11 @@ void loop()
 
   unsigned int signalMax = 0;    // minimum value
   unsigned int signalMin = 1024; // maximum value
+
+  if (!client.connected())
+  {
+    reconnect();
+  }
 
   // collect data for 50 mS
   while (millis() - startMillis < sampleWindow)
@@ -131,9 +149,9 @@ void loop()
   peakToPeak = signalMax - signalMin;          // max - min = peak-peak amplitude
   int db = map(peakToPeak, 20, 900, 49.5, 90); // calibrate for deciBels
 
-  Serial.print("Loudness: ");
-  Serial.print(db);
-  Serial.println("dB");
+  // Serial.print("Loudness: ");
+  // Serial.print(db);
+  // Serial.println("dB");
 
   // ? This is OLD Code to turn on LIGHTBAR!
   /*if ((db > 65) && (db < 80))
@@ -185,35 +203,42 @@ void loop()
 */
 
   // ? This is a NEW Code to turn on LIGHTBAR!
-  if(db>=80){
-    LightBar(1,1,1,1);
+  if (db >= 80)
+  {
+    LightBar(1, 1, 1, 1);
     tone(Buzzer, 4000, 1000);
   }
-  else if((db>=65)&&(db<80)){
-    LightBar(1,1,1,0);
+  else if ((db >= 65) && (db < 80))
+  {
+    LightBar(1, 1, 1, 0);
   }
-  else if((db>60)&&(db<65)){
-    LightBar(1,1,0,0);
+  else if ((db > 60) && (db < 65))
+  {
+    LightBar(1, 1, 0, 0);
   }
-  else if((db>50)&&(db<=60)){
-    LightBar(1,0,0,0);
+  else if ((db > 50) && (db <= 60))
+  {
+    LightBar(1, 0, 0, 0);
   }
-  else {
-    LightBar(0,0,0,0);
+  else
+  {
+    LightBar(0, 0, 0, 0);
   }
 
   delay(50);
 
   // ? Sending Data to NETPIE
-  if(startMillis - lastMsg > 5000){
+  if (startMillis - lastMsg > 5000)
+  {
     lastMsg = startMillis;
     ++value;
-    DataString = "{\"data\":{\"Loudness\" : "+(String)db+"}}";
+    DataString = "{\"data\":{\"Loudness\":" + (String)db + "}}";
     DataString.toCharArray(msg, 100);
-    Serial.println("Hello NETPIE2020");
+    Serial.println("Hello Node-Red");
     Serial.print("Publish message : ");
     Serial.println(msg);
-    client.publish("sound/status", msg);
+    client.publish("SoundSensor/publish", msg);
   }
   delay(1);
+  client.loop();
 }
